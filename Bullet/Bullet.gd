9cd4797566792a0faadd5472eb3dec4be5e8f056
0384359 <0384359@schoolsnet.act.edu.au>
2023-03-01 09:30:15 +1100

    touchups

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -5,6 +5,7 @@ var speed = 500
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	GlobalVariables.bulletInstanceCount += 1
 	set_physics_process(true)
 
 func _physics_process(delta):
@@ -15,3 +16,4 @@ func _physics_process(delta):
 			collidedObject.get_collider().queue_free()
 			GlobalVariables.scoringInformation["currentScore"] +=10
 		queue_free()
+		GlobalVariables.bulletInstanceCount -= 1
