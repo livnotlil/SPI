a5ebf312d68eb06d33e36aad78397a7ebb16bd00
0384359 <0384359@schoolsnet.act.edu.au>
2023-05-12 12:56:27 +1000

    enemy movement

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -11,11 +11,11 @@ func _ready():
 func _colliding(area):
 	if area.is_in_group("right"):
 		#print("emenies collide right")
-		get_parent().global_position.y += 10
+		get_parent().global_position.x += 10
 		get_parent().speed = -200
 	if area.is_in_group("left"):
 		#print("emenies collide left")
-		get_parent().global_position.y += 10
+		get_parent().global_position.x += 10
 		get_parent().speed = 200
 
 
